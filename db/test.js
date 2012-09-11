6d43260bb9c6ec56b3abb6c67056e270e63a488d
syshen <sysheen@gmail.com>
2012-09-11 16:37:21 +0800

    ui

--- db/test.js
+++ db/test.js
@@ -3,7 +3,7 @@
 var ldquery = require('./LDquery')();
 
 
-ldquery.postsNew({ 'owner': '123456', 'species': 'dog', 'name': 'coffee', 'reward': 'a kiss', 'breed': 'Shina Inu', 'gender': 'M', 'color': 'brown', 'place': {'lat': 123456, 'lon': 789012}, 'photos': []}, function() {
+ldquery.postsNew({ 'owner': '123456', 'species': 'dog', 'name': 'coffee', 'reward': 'a kiss', 'breed': 'Shina Inu', 'gender': 'M', 'color': 'brown', 'place': {'lat': 25.044198, 'lon': 121.551533}, 'photos': []}, function() {
 
     ldquery.postsListAll(function(err, objs) {
 
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var cons = require('consolidate');
 var express = require('express');
 var util = require('sys');
+var dbQuery = require('../db/LDquery')();
 
 //Initialize upload folder
 var uploadFolder = __dirname + '/uploads'
@@ -37,10 +38,14 @@ app.set('views', __dirname + '/views');
 //Routing
 //Index
 app.get('/', function(req,res) {
-    res.render('index.ejs', {
-        req: req,
-        app: app,
-        app_id: process.env.FACEBOOK_APPID,
+
+    dbQuery.postsListAll(function(err, records) {
+        res.render('index.ejs', {
+            req: req,
+            app: app,
+            app_id: process.env.FACEBOOK_APPID,
+            records: records
+        });
     });
 });
 
